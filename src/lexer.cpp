1fc90d7645da9bdc2c2ae5d29bc24520601b60d4
Rafael Sá Menezes <rafael@localhost.localdomain>
2016-07-07 18:29:31 -0400

    Refatorações no getToken

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,4 +1,4 @@
-#include "../lexer.hpp"
+#include "../header/lexer.hpp"
 
 
 /*Função mágica pra identificar o token de uma palavra
@@ -10,25 +10,57 @@
 */
 
 void Lexer::getToken(std::string word) {
-  static std::regex Type("(bool)|(int)");
-  static std::regex Identifier("[:alpha:][:w:]+"); // w = alphanum
-  static std::regex DeclOp(":");
-  static std::regex EndCommand(";");
-  static std::regex DeclOp(":");
   static std::regex Integer("[:d:]+");
-  static std::regex Bool("(false)|(true)");
+  static std::regex Identifier("[:alpha:][:w:]+"); // w = alphanum
+
+  if (std::regex_match(Iteger, word)) {
+    this->NumVal = std::atoi(word);
+    this->last   = Token::Integer;
+  }
+  else if(std::regex_match(Identifier, word)) {
+    //if's aninhados... :(
+    else if (!word.compare(""))
+
+
+  )
+  else {
+    if(!word.compare("+")) {
+
+    }
+    else if (!word.compare("*")) {
+
+    }
+    else if (!word.compare("=")) {
+
+    }
+    else if (!word.compare("<=")) {
+
+    }
+    else if (!word.compare("==")) {
+
+    }
+
+
+  }
+
+  }
   static std::regex PlusOp("\\+");
   static std::regex TimesOp("\\*");
   static std::regex AssignOp("=");
   static std::regex LessEq("<=");
   static std::regex Equal("==");
   static std::regex If("if");
+  static std::regex EndCommand(";");
+  static std::regex DeclOp(":");
   static std::regex Then("then");
   static std::regex Else("else");
+  static std::regex Bool("(false)|(true)");
   static std::regex While("while");
   static std::regex Do("do");
+  static std::regex Type("(bool)|(int)");
   static std::regex Decl("decl");
   static std::regex Begin("begin");
   static std::regex End("end");
+
 
 }
